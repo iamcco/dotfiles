1318bfe2206b0e9cce8bff25d33be0fbf8bde81e
年糕小豆汤 <ooiss@qq.com>
2016-09-18 01:32:19 +0800

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# consts
 SCRIPT_PATH=${0%\/*}
 cd $SCRIPT_PATH
 SCRIPT_PATH=$PWD
@@ -11,37 +12,9 @@ BLUE='\033[0;34m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-mkdir_backup() {
-    mkdir "$BACKUP_DIR"
-    printf "make directory $CYAN$BACKUP_DIR$NC\n"
-}
+# install softwares
+source $SCRIPT_PATH/sh/softwares
+# link dotfiles
+source $SCRIPT_PATH/sh/setup_dotfiles
 
-mk_symlink() {
-    if [ ! $1 ]
-    then
-        printf "$RED mk_symlink need a name $NC\n"
-    else
-        local TARGET=$SCRIPT_PATH/$1
-        local LINKTO=$HOME/.$1
-        if [ -e "$TARGET" ]
-        then
-            if [ -e $LINKTO -o -L $LINKTO ]
-            then
-                if [ ! -d $BACKUP_DIR ]; then mkdir_backup; fi
-                mv "$LINKTO" "$BACKUP_DIR/"
-                printf "backup $CYAN$LINKTO$NC -> $CYAN$BACKUP_DIR$NC\n"
-            fi
-            ln -s "$TARGET" "$LINKTO"
-            printf "linked $CYAN$TARGET$NC -> $CYAN$LINKTO$NC\n"
-        else
-            printf "file $RED$TARGET$NC is not exists\n"
-        fi
-    fi
-}
-
-
-for ITEM in config/nvim config/nyaovim eslintrc gitconfig tern-project tmux.conf zshrc bin
-do
-    mk_symlink $ITEM
-done
-
+# vim:set ft=sh:
