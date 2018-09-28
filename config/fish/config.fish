baeb88c666e88c7146ec118520085bd47b1b3a5b
iamcco <ooiss@qq.com>
2018-09-28 15:49:53 +0800

    update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -49,4 +49,4 @@ set proxy_auth false
 if [ -f $HOME'/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source $HOME'/google-cloud-sdk/path.fish.inc'; else; . $HOME'/google-cloud-sdk/path.fish.inc'; end; end
 
 # pj plugin
-set -gx PROJECT_PATHS ~/workspace/pxn ~/.configrc
+set -gx PROJECT_PATHS ~/ ~/workspace/pxn ~/development
